e709c6f4caae8defc2886b5e98adc5173486f874
katarzyna_w <katarzyna.koch@gmail.com>
2021-01-14 17:44:18 -0600

    Added arrays with electoral votes

--- actions.js
+++ actions.js
@@ -10,4 +10,8 @@ var biden = createPolitician("Joe Biden");
 
 var trump = createPolitician("Donald Trump");
 
-console.log(biden);
+biden.electionResults = [0, 0, 11, 0, 50, 9, 7, 3, 3, 10, 16, 4, 0, 20, 0, 0, 0, 0, 0, 3, 10, 11, 16, 10, 0, 0, 0, 1, 6, 4, 14, 5, 29, 0, 0, 0, 0, 7, 20, 4, 0, 0, 0, 10, 0, 3, 13, 12, 0, 10, 0];
+
+trump.electionResults = [9, 3, 0, 6, 5, 0, 0, 0, 0, 19, 0, 0, 4, 0, 11, 6, 6, 8, 8, 1, 0, 0, 0, 0, 6, 10, 3, 4, 0, 0, 0, 0, 0, 15, 3, 18, 7, 0, 0, 0, 9,  3, 11, 28, 6, 0, 0, 0, 5, 0, 3];
+
+console.log(biden.electionResults);
