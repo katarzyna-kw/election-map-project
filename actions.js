4b2afff63b86828d1c7d61242065e9f503efa7eb
katarzyna_w <katarzyna.koch@gmail.com>
2021-01-14 18:02:11 -0600

    Added party colors

--- actions.js
+++ actions.js
@@ -1,6 +1,7 @@
-var createPolitician = function(name) {
+var createPolitician = function(name, partyColor) {
     var politician = {};
      politician.name = name;
+     politician.partyColor = partyColor;
      politician.electionResults = null;
      politician.totalVotes = 0;
    
@@ -15,9 +16,9 @@ var createPolitician = function(name) {
      return politician;
    }
    
-   var biden = createPolitician("Joe Biden");
+   var biden = createPolitician("Joe Biden", [245, 141, 136]);
    
-   var trump = createPolitician("Donald Trump");
+   var trump = createPolitician("Donald Trump", [132, 17, 11]);
    
    biden.electionResults = [0, 0, 11, 0, 50, 9, 7, 3, 3, 10, 16, 4, 0, 20, 0, 0, 0, 0, 0, 3, 10, 11, 16, 10, 0, 0, 0, 1, 6, 4, 14, 5, 29, 0, 0, 0, 0, 7, 20, 4, 0, 0, 0, 10, 0, 3, 13, 12, 0, 10, 0];
    
@@ -45,7 +46,5 @@ var createPolitician = function(name) {
        winner = "TIE."
      }
    
-   console.log("And the winner is..." + winner + "!!!");
-   console.log(biden.totalVotes);
-   console.log(trump.totalVotes);
+   console.log("Joe Biden's color is " + biden.partyColor);
    
